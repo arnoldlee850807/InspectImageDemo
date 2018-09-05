b0a689ed23535403daff175b9ac1e45c6a6ae939
Arnold Lee <arnold.kn.lee@gmail.com>
2018-09-05 14:54:30 +0800

    Initial Commit

---
+++ InspectImageDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InspectImageDemo
+//
+//  Created by i_skyhung on 2018/9/5.
+//  Copyright © 2018年 Arnold Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
